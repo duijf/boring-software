61017a80a5996f0bf491eeb5bc7ffbf4cdc1721b
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2019-06-19 12:21:00 +0200

    Publish executeSqlFile

--- code/src/Cli.hs
+++ code/src/Cli.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cli
   ( main
   ) where
 
+import qualified Data.ByteString as Bs
 import qualified Database.PostgreSQL.Simple as Pg
+import qualified Database.PostgreSQL.Simple.Types as PgTypes
+
 
 main :: IO ()
 main = do
@@ -17,6 +21,13 @@ main = do
 
   conn <- Pg.connect connInfo
 
-  putStrLn "hi"
+  executeSqlFile conn "db/000_bootstrap.sql.up"
 
+  pure ()
+
+
+executeSqlFile :: Pg.Connection -> FilePath -> IO ()
+executeSqlFile conn filePath = do
+  contents <- PgTypes.Query <$> Bs.readFile filePath
+  Pg.execute_ conn contents
   pure ()
